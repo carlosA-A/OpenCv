db20a8a91fdef8db7266b2ddf2812269e0803f74
Ubuntu2 <c.avogadro@ufl.edu>
2016-06-02 14:26:16 -0400

    Added a tiemer that displays the time elapsed to complete process

--- C++/imageTest/smoothImage/smooth_image.cpp
+++ C++/imageTest/smoothImage/smooth_image.cpp
@@ -6,10 +6,22 @@ using namespace std;
 using namespace cv;
 
 int main(int argc, char const *argv[]) {
+
+  double t0 = (double)getTickCount();
+
   Mat img_rgb = imread(argv[1],CV_LOAD_IMAGE_COLOR);
   Mat img_gry, img_cny,img_pyr,img_pyr2;
+  Vec3b pixel1 = img_rgb.at<Vec3b>(0,0);
+  cout << "Value of pixel (0,0): " << (unsigned int)pixel1[1] << endl;
+  std::cout << "Image channels RGB: "<< img_rgb.channels() << std::endl;
+  std::cout << "Image depth RGB: "<< img_rgb.depth() << std::endl;
+
 
   cvtColor(img_rgb, img_gry,COLOR_BGR2GRAY);
+  std::cout << "Image channels GrayScale: "<< img_gry.channels() << std::endl;
+  std::cout << "Image depth GrayScale: "<< img_gry.depth() << std::endl;
+
+
   pyrDown(img_gry, img_pyr);
   pyrDown(img_pyr,img_pyr2);
   namedWindow("Example Gray", WINDOW_AUTOSIZE);
@@ -20,7 +32,9 @@ int main(int argc, char const *argv[]) {
 
   imshow("Example Canny", img_cny);
 
+  double elapsed = ((double)getTickCount() - t0)/getTickFrequency();
 
+  std::cout << "Time elapsed: "<<elapsed << std::endl;
   waitKey(0);
 
   return 0;
