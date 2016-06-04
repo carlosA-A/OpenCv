936c0b4faa42bf1531fe0d937c4a22426f9d0047
Ubuntu2 <c.avogadro@ufl.edu>
2016-06-04 17:55:23 -0400

    Detect red pieces in a checker board

---
+++ C++/imageTest/morphological_operations/morph_operations.cpp
@@ -0,0 +1,28 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char const *argv[]) {
+
+  Mat img = imread(argv[1]);
+  Mat dst1,dst2,dst3,dst4;
+
+  //Range to detect color red in image
+  inRange(img, Scalar(0,0,100), Scalar(30,40,255), dst1);
+  //Kernel to help detect a circle of around 15 pixels by 15 in image
+  Mat element = getStructuringElement(MORPH_ELLIPSE, Size(15,15));
+
+  //Dilate image
+  dilate(dst1, dst2, element);
+  //Erode image
+  erode(dst2,dst3,element);
+
+  imshow("Original", img);
+  imshow("Range", dst1);
+  imshow("Dilate", dst2);
+  imshow("Eroded", dst3);
+  waitKey(0);
+  return 0;
+}
