082b70d43e0c6fac01028c1801f58324fabbc560
Ubuntu2 <c.avogadro@ufl.edu>
2016-06-05 17:39:09 -0400

    Added code to rotate image

--- C++/imageTest/geometric_transformations/translation/translation.cpp
+++ C++/imageTest/geometric_transformations/translation/translation.cpp
@@ -15,6 +15,7 @@ int main(int argc, char const *argv[]) {
 
   }
   Mat M =(Mat_<double>(2,3)<<1,0,200,0,1,150);  //Create a 2-by-3 array with the values for the translation, 200 pixels displaced horizontally, and 150 vertically
+  //Mat R = getRotationMatrix2D(Point2f(img.cols/2.0,img.rows/2.0), 90, 1); //get a 2X3 matrix to rotate our image 90 degrees
   warpAffine(img, dst, M, img.size());
 
   imshow("Original", img);
