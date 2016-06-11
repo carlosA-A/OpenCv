67086b57b83016cce005b09e8c7e32af6d3e3355
Ubuntu2 <c.avogadro@ufl.edu>
2016-06-11 13:40:09 -0400

    Trying to display separated hsv in color

--- C++/imageTest/hsv_conversion/hsv_conversion.cpp
+++ C++/imageTest/hsv_conversion/hsv_conversion.cpp
@@ -10,6 +10,7 @@ vector<Mat>  showSeparatedChannels(vector<Mat> channels);
 int main(int argc, char const *argv[]) {
 
   Mat img = imread(argv[1]);
+  Mat dst;
 
   if(!img.data){
     std::cout << "Error opening image" << std::endl;
@@ -19,6 +20,49 @@ int main(int argc, char const *argv[]) {
 
   imshow("Original", img);
 
+  cvtColor(img, dst, COLOR_RGB2HSV);
 
+  std::vector<Mat> v;
+
+  split(dst, v);
+
+  imshow("Hue", v[0]);
+  imshow("Saturation", v[1]);
+  imshow("Value", v[2]);
+
+  vector<Mat> separatedChannels = showSeparatedChannels(v);
+
+  imshow("Hue Cl", separatedChannels[0]);
+  imshow("Saturation Cl", separatedChannels[1]);
+  imshow("Value Cl", separatedChannels[2]);
+
+
+  waitKey(0);
+
+
+
+}
+
+vector<Mat>  showSeparatedChannels(vector<Mat> channels){
+  vector<Mat> separatedChannels;
+
+  Mat zer = Mat::zeros(channels[0].size(), channels[0].type());
+  for (int i = 0; i < 3; i++) {
+    vector<Mat> aux;
+    Mat dst;
+    for (int j = 0; j < 3; j++) {
+
+      if(i==j){
+        aux.push_back(channels[i]);
+      }
+      else{
+
+        aux.push_back(zer);
+      }
+    }
+    merge(aux, dst);
+    separatedChannels.push_back(dst);
+  }
+  return separatedChannels;
 
 }
